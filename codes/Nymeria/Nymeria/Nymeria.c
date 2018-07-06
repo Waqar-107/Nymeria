5399e48aacd94b0396572b3c1730f16e0c67396f
Waqar-107 <waqar.hassan866@gmail.com>
2018-07-06 13:34:06 +0600

    added the receiver-part circuit diagram and updated code

--- codes/Nymeria/Nymeria/Nymeria.c
+++ codes/Nymeria/Nymeria/Nymeria.c
@@ -6,42 +6,53 @@
 #include <util/delay.h>
 
 //---------------------------------------------------------
+//in1,in2,in3 and in4 connected with D0,D1,D2 and D3 OF the RF receiver 
 //in1,in2 for right pairs- when in1=1,in2=0 then clockwise and when in1=0,in2=1 then anti-clockwise
 //in3,in4 for left pairs- when in3=1,in4=0 then clockwise and when in3=0,in4=1 then anti-clockwise
-void forward() 
-{
+//---------------------------------------------------------
+void sendData(unsigned char direction) {
+	//using PORTD for this purpose
+	PORTD=direction;
+}
+
+void forward() {
 	//in4 in3 in2 in1 - 0101 - both pairs in clockwise
-	PORTC=0b00000101;
+	unsigned char direction=0b00000101;
+	sendData(direction);
 }
 
-void backward()
-{
+void backward() {
 	//in4 in3 in2 in1 - 1010 - both pairs in counter-clockwise
-	PORTC=0b00001010;
+	unsigned char direction=0b00001010;
+	sendData(direction);
 }
 
-void left()
-{
+void left() {
 	//in4 in3 in2 in1 - 0001- right-pairs in clockwise, left pairs stopped
-	PORTC=0b00000001;
+	unsigned char direction=0b00000001;
+	sendData(direction);
 }
 
-void right()
-{
+void right() {
 	//in4 in3 in2 in1 - 0100- left pairs in clockwise, right pairs stopped
-	PORTC=0b00000100;
+	unsigned char direction=0b00000100;
+	sendData(direction);
+}
+
+void stop() {
+	//in4 in3 in2 in1 - 0000 - stop the wheels
+	unsigned char direction=0b00000000;
+	sendData(direction);
 }
 //---------------------------------------------------------
 
 int main(void)
 {
-	DDRC=0xFF;
-	int dir=1;
+	DDRD=0xFF;
 	
     while(1)
     {
-        //in4 in3 in2 in1
-		
-		PORTC=0b00000101;
+        //write necessary codes, use ADC and decide which way to goto
+		_delay_ms(500);
     }
 }
