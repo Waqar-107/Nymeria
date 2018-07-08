26ec38a2a120b0662c55c5317cd6f4209f992dd2
Waqar-107 <waqar.hassan866@gmail.com>
2018-07-08 21:50:07 +0600

    updated code, fixed bug in adc

--- codes/Nymeria/Nymeria/Nymeria.c
+++ codes/Nymeria/Nymeria/Nymeria.c
@@ -50,13 +50,12 @@ void stop() {
 
 //---------------------------------------------------------
 void adc_init() {
-	ADMUX |= (1<<REFS0);
+	ADMUX = (1<<REFS0);
 	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
 }
 
 uint16_t read_channel(unsigned char channel) {
-	channel=0b00000111;
-	ADMUX |= channel;
+	ADMUX = (1<<REFS0) | channel;
 	
 	//start conversion
 	ADCSRA |= (1<<ADSC);
